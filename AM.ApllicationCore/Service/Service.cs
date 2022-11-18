14aee7b71d088868c7cbd08339e1d9cf13de24e5
mimi667 <mariem.aljene@esprit.tn>
2022-11-18 15:09:49 +0100

    tp7

---
+++ AM.ApllicationCore/Service/Service.cs
@@ -0,0 +1,53 @@
+﻿using AM.ApllicationCore.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApllicationCore.Service
+{
+    public class Service<TEntity> : IServices<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Expression<Func<TEntity, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
